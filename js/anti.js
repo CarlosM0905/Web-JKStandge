b681bf4c3e7f1148f085bf4868d0b6d6e59e30d0
CarlosM0905 <carlos1843216@gmail.com>
2019-07-25 20:07:30 -0500

    Antirobo diseños

---
+++ js/anti.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    
+
+    document.getElementById("lista-stands").addEventListener("mousedown",function(event){
+        console.log(event);
+        if(event.which == 3) 
+             {
+                alert("Derechos Reservados - JKStandge 2019");
+             }
+       });
+
+      
+});
